896365ead8dab616e2f8647d231b3a4e363d34f2
ansalim01 <ansalim14@gmail.com>
2023-01-30 12:24:08 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,32 @@
+'use strict'
+
+
+function ibg() {
+
+    let ibg = document.querySelectorAll("._ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+        }
+    }
+}
+
+ibg();
+
+new Swiper('.swiper-main-header__container', {
+    navigation: {
+        nextEl: '.arrows-swiper-main__arrows-right',
+        prevEl: '.arrows-swiper-main__arrows-left',
+    },
+    pagination: {
+        el: '.arrows-swiper-main__pagination',
+        clickable: true,
+        type: 'fraction',
+    },
+
+    grabCursor: true,
+    touchAngle: 40,
+    loop: true,
+});
+
+
